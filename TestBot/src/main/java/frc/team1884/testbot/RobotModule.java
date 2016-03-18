086e6aae96c7697b089cb130833a74e7fe9dafb9
Will V <will_vauclain@asl.org>
2016-03-18 18:32:48 +0000

    Added TestBot Project

---
+++ TestBot/src/main/java/frc/team1884/testbot/RobotModule.java
@@ -0,0 +1,25 @@
+package frc.team1884.testbot;
+
+import jaci.openrio.toast.lib.log.Logger;
+import jaci.openrio.toast.lib.module.IterativeModule;
+
+public class RobotModule extends IterativeModule {
+
+    public static Logger logger;
+
+    @Override
+    public String getModuleName() {
+        return "TestBot";
+    }
+
+    @Override
+    public String getModuleVersion() {
+        return "0.0.1";
+    }
+
+    @Override
+    public void robotInit() {
+        logger = new Logger("TestBot", Logger.ATTR_DEFAULT);
+        //TODO: Module Init
+    }
+}
